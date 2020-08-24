62384e3022b2e4cab0c53c720fe6eaaf952cc048
Caryn Ligon <cligon@thezebra.com>
2020-08-24 11:50:40 -0500

    fixing title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Derrick Ligon',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -52,6 +52,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) { }
+    extend(config, ctx) {}
   }
 }
